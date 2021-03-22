7a89c9a20cd6fd1fb42727b3d1b18efa804501e0
moise7 <watibonheur@gmail.com>
2021-03-22 04:37:01 -0400

    up

---
+++ ToDoList/Models/ToDo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ToDoList.Models
+{
+    public class ToDo
+    {
+        public  int Id { get; set; }
+        public string Description { get; set; }
+        [Display(Name = "DueDate")]
+        [DataType(DataType.Date)]
+        public string  Date { get; set; }
+        public bool Done { get; set; }
+        public virtual ApplicationUser User { get; set; }
+    }
+}
